c35f2c3c106e5b5140bb6e1b9e50d1bf7e5dc73e
samgrowthlabs-cloud <samgrowthlabs@gmail.com>
2026-08-16 02:56:31 -0300

    destacar os cards de alternativas proximas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='shoplab-pwa-v8-search-navigation';
+const VERSION='shoplab-pwa-v9-alternatives-curadoria';
 const SHELL=['/','/index.html','/offline.html','/manifest.webmanifest','/assets/css/main.css','/assets/img/favicon.svg','/assets/img/shoplab-wordmark.png','/assets/img/pwa-maskable.svg','/assets/js/pwa.js','/assets/js/mobile-enhancements.js'];
 
 self.addEventListener('install',event=>{
