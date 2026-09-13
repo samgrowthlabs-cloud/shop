2257810f9e28645a982ef59a0380fa165cee8bbb
samgrowthlabs-cloud <samgrowthlabs@gmail.com>
2026-09-13 01:36:19 -0300

    colocar o shoplab ads na pagina de noticias

--- assets/js/app-20260827-v5.js
+++ assets/js/app-20260827-v5.js
@@ -7,7 +7,7 @@ import{session as authSession,currentUser,signOut,startPresence,userApi}from'./a
 import{bindLibraryUI,syncAccountLibrary,localLibrary,getPersonalizedRecommendations}from'./user-library-20260827-v5.js';
 import{cachedPremiumBrand,setPremiumBrand}from'./site-header.js?v=20260726-mobile-plus-logo-1';
 import{SHOPLAB_CONFIG}from'./config.js?v=20260803-media-domain-38';
-import{mountShoplabAds}from'./shoplab-ads-public.js?v=20260913-search-compact-9x6-2';
+import{mountShoplabAds}from'./shoplab-ads-public.js?v=20260913-search-mobile-fullwidth-1';
 import{renderHomeBanner,renderHeaderHighlight}from'./visual-renderers.js?v=20260821-header-spotlight-carousel-1';
 import{initUniversalSearch}from'./universal-search.js?v=20260913-compact-headings-2';
 import{selectAutomaticComparisons,automaticComparisonSection}from'./automatic-comparisons.js?v=20260820-home-cards-1';
