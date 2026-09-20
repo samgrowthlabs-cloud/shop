52302d7c916dc8b787cf3eb033ec6aa71a4a7f8d
samgrowthlabs-cloud <samgrowthlabs@gmail.com>
2026-09-20 01:01:00 -0300

    aumentar a velocidade do site

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='shoplab-v3-mobile-header';
+const VERSION='shoplab-v4-mobile-header-geometry';
 const STATIC_CACHE=`${VERSION}-static`,PUBLIC_CACHE=`${VERSION}-public`;
 const SHELL=['/offline.html','/assets/img/favicon.svg','/assets/img/shoplab-wordmark.png'];
 const PUBLIC_API=/^\/api\/v1\/(?:home|products(?:\/[^/]+)?|categories(?:\/weekly-highlights)?|promotions|collections(?:\/[^/]+)?|search|site-config)$/;
