af2939d9b0d5478558b08419ca9fac619ca9a491
samgrowthlabs-cloud <samgrowthlabs@gmail.com>
2026-09-22 19:10:36 -0300

    deixar tudo mais rapido

--- assets/js/app-20260827-v5.js
+++ assets/js/app-20260827-v5.js
@@ -1,4 +1,4 @@
-import'./favicon.js?v=20260920-mobile-header-stable-3';import{getProducts,getTrendingProducts,getCategories,getWeeklyCategoryHighlights,getPromotions,getCollection,getProductBySlug,prefetchProduct,prefetchProductMedia,searchProducts,searchProductsWithMeta,getRecommendations,getFeaturedCollections,getSiteConfig,getHomeData,cachedHomeData,cachedSiteConfig,trackEvent}from'./api.js?v=20260922-banner-instant-1';
+import'./favicon.js?v=20260920-mobile-header-stable-3';import{getProducts,getTrendingProducts,getCategories,getWeeklyCategoryHighlights,getPromotions,getCollection,getProductBySlug,prefetchProduct,prefetchProductMedia,searchProducts,searchProductsWithMeta,getRecommendations,getFeaturedCollections,getSiteConfig,getHomeData,cachedHomeData,cachedSiteConfig,trackEvent}from'./api.js?v=20260922-home-swr-1';
 import'./search-ui.js?v=20260906-search-history-1';
 import{session as authSession,currentUser,signOut,startPresence,userApi}from'./auth-20260827-v5.js?v=20260922-invalid-refresh-cleanup-2';
 import{bindLibraryUI,syncAccountLibrary,localLibrary,getPersonalizedRecommendations}from'./user-library-20260827-v5.js';
